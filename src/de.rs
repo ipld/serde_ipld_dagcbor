8aa70690bceb750676ad95bf3fe735bd35fa76df
Volker Mische <volker.mische@gmail.com>
2026-05-18 14:38:03 +0200

    fix: move decrement after error

--- src/de.rs
+++ src/de.rs
@@ -715,12 +715,12 @@ impl<'de, R: dec::Read<'de>> de::MapAccess<'de> for Accessor<'_, R> {
     {
         if self.len > 0 {
             let name = "map key";
-            self.len -= 1;
             // Map keys must be strings in DAG-CBOR.
             let byte = peek_one(name, &mut self.de.reader)?;
             if dec::if_major(byte) != major::STRING {
                 return Err(DecodeError::Mismatch { name, found: byte });
             }
+            self.len -= 1;
             Ok(Some(seed.deserialize(&mut *self.de)?))
         } else {
             Ok(None)
