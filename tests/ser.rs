a50d1b0362266de59663b1fbaced36eb33c3422e
Volker Mische <volker.mische@gmail.com>
2026-07-17 11:43:48 +0200

    chore: remove print statements from tests (#63)
    Print statements make sense for debugging, but else they are just noise.
    Also one of them lead to a Clippy warning. Hence removing all of them.

--- tests/ser.rs
+++ tests/ser.rs
@@ -124,7 +124,6 @@ fn test_ip_addr() {
 
     let addr = Ipv4Addr::new(8, 8, 8, 8);
     let vec = to_vec(&addr).unwrap();
-    println!("{:?}", vec);
     assert_eq!(vec.len(), 5);
     let test_addr: Ipv4Addr = from_slice(&vec).unwrap();
     assert_eq!(addr, test_addr);
